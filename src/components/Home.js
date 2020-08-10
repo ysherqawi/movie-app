b0fc7105c92862c63c474e2d340c828f3fc13756
ysherqawi <ysherqawi@gmail.com>
2020-08-11 02:10:40 +0300

    added useHomeFetch custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,15 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import HeroImage from './elements/HeroImage';
 import SearchBar from './elements/SearchBar';
 import Grid from './elements/Grid';
 import MovieThump from './elements/MovieThump';
 import Spinner from './elements/Spinner';
 import LoadMoreBtn from './elements/LoadMoreBtn';
-import {
-  API_URL,
-  API_KEY,
-  IMAGE_BASE_URL,
-  POSTER_SIZE,
-  BACKDROP_SIZE,
-} from '../config/config';
+import { useHomeFetch } from './hooks/useHomeFetch';
 
 const Home = () => {
-  const [state, setstate] = useState({ movies: [] });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (endpoint) => {
-    setError(false);
-    setLoading(true);
-
-    try {
-      const result = await (await fetch(endpoint)).json();
-      console.log(result);
-      setstate((prev) => ({
-        ...prev,
-        movies: [...result.results],
-        heroImage: prev.heroImage || result.results[0],
-        currentPage: result.page,
-        totalPages: result.total_pages,
-      }));
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
-  }, []);
-
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
   return (
     <Fragment>
       <HeroImage />
