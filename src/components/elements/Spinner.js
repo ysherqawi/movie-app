dd63efd04723764880567e6bdb0092930fafe191
ysherqawi <ysherqawi@gmail.com>
2020-08-11 21:29:29 +0300

    created and styled spinner component

--- src/components/elements/Spinner.js
+++ src/components/elements/Spinner.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import { StyledSpinner } from './../styles/StyledSpinner';
 
-const Spinner = () => {
-  return <div>Spinner</div>;
-};
+const Spinner = () => <StyledSpinner />;
 
 export default Spinner;
