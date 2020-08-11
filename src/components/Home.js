c5b1c1eb46dd7ec6c0b5ed542703b8cf7098741e
ysherqawi <ysherqawi@gmail.com>
2020-08-11 18:33:03 +0300

    created and styled heroimage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,22 @@ import MovieThump from './elements/MovieThump';
 import Spinner from './elements/Spinner';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import { useHomeFetch } from './hooks/useHomeFetch';
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../config/config';
 
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
   console.log(state);
+
+  if (error) return <div>Somthing went wrong...</div>;
+  if (!state.movies[0]) return <Spinner />;
+
   return (
     <Fragment>
-      <HeroImage />
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <MovieThump />
