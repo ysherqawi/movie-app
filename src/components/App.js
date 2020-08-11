55201d5b481512d6bc573b9305f373ca9bcbe49a
ysherqawi <ysherqawi@gmail.com>
2020-08-11 18:42:32 +0300

    added global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './elements/Header';
 import Home from './Home';
+import { GlobalStyle } from './styles/GlobalsStyle';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Home />
-    </div>
+      <GlobalStyle />
+    </>
   );
 }
 
